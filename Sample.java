8b7ac62f7e8d912ad86974d63191149c5f93c131
unknown <11460870@057LAB02WKS03.Campus.ittesi.com>
2012-08-10 21:46:12 -0400

    Charlie Changed

--- Sample.java
+++ Sample.java
@@ -3,5 +3,10 @@ public class Sample {
   public static void main(String [] args) {
     System.out.println("Hello World");
   }
+  public int CharliesAwesomelyAmazinglyLongMethodName(){
+	  int i = 0;
+	  i =(int) ((int)i + Math.random() *100);
+	  return i;
+  }
   
 }
